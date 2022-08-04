32d48e0b7b525d4fcb1ba1658401f705477708bd
cedric arnaud <79386450+CedricArnaud20@users.noreply.github.com>
2022-08-03 23:55:10 -0300

    refatoração inglês

--- burger-eats/cypress/integration/cadastro.spec.js
+++ burger-eats/cypress/integration/cadastro.spec.js
@@ -9,48 +9,48 @@ describe('Cadastro', ()=>{
         cy.get('a[href="/deliver"]').click()
         cy.get('#page-deliver form h1').should('have.text' , 'Cadastre-se para  fazer entregas')
 
-        var entregador = {
-            nome: 'James Rodriguez',
+        var deliver = {
+            name: 'James Rodriguez',
             cpf: '00000001441',
             email:'james@gmail.com',
-            numero: '62982142744',
-            endereco: {
-                cep: '75083460',
-                rua: 'Rua Doutor Evandro Pinto Silva',
-                numero: '20',
-                bairro: 'Cidade Universitária',
-                complemento: 'AP 23',
-                cidade_uf: 'Anápolis/GO'
+            number: '62982142744',
+            address: {
+                postalcode: '75083460',
+                street: 'Rua Doutor Evandro Pinto Silva',
+                number: '20',
+                details: 'Cidade Universitária',
+                district: 'AP 23',
+                city_state: 'Anápolis/GO'
             },
 
-            metodo_entrega: 'Moto',
+               deliver_method: 'Moto',  
             cnh:'CNH-E.png'
 
         }
 
-        cy.get('input[name="name"]').type(entregador.nome)
-        cy.get('input[name="cpf"]').type(entregador.cpf)
-        cy.get('input[name="email"]').type(entregador.email)
-        cy.get('input[name="whatsapp"]').type(entregador.numero)
+        cy.get('input[name="name"]').type(deliver.name)
+        cy.get('input[name="cpf"]').type(deliver.cpf)
+        cy.get('input[name="email"]').type(deliver.email)
+        cy.get('input[name="whatsapp"]').type(deliver.number)
 
-        cy.get('input[name="postalcode"]').type(entregador.endereco.cep)
+        cy.get('input[name="postalcode"]').type(deliver.address.postalcode)
         cy.get('input[value="Buscar CEP"]').click()
         cy.wait(2000)
 
-        cy.get('input[name="address-number"]').type(entregador.endereco.numero)
-        cy.get('input[name="address-details"]').type(entregador.endereco.complemento)
+        cy.get('input[name="address-number"]').type(deliver.address.number)
+        cy.get('input[name="address-details"]').type(deliver.address.details)
 
-        cy.get('input[name="address"]').should('have.value',entregador.endereco.rua)
-        cy.get('input[name="district"]').should('have.value',entregador.endereco.bairro)
-        cy.get('input[name="city-uf"]').should('have.value',entregador.endereco.cidade_uf)
+        cy.get('input[name="address"]').should('have.value',deliver.address.street)
+        cy.get('input[name="district"]').should('have.value',deliver.address.details)
+        cy.get('input[name="city-uf"]').should('have.value',deliver.address.city_state)
 
 
-        cy.contains('.delivery-method li', entregador.metodo_entrega).click()
+        cy.contains('.delivery-method li', deliver.deliver_method).click()
         // cy.get('ul>li>[alt="Moto"]').click()
         // cy.get('ul>li>[alt="Bicicleta"]').click()
         // cy.get('ul>li>[alt="Van/Carro"]').click()
 
-        cy.get('input[accept^="image"]').attachFile(entregador.cnh)
+        cy.get('input[accept^="image"]').attachFile(deliver.cnh)
 
         cy.get('form button[type="submit"]').click()
 
@@ -62,55 +62,55 @@ describe('Cadastro', ()=>{
     })
 
     
-    it('CPF incorreto', ()=>{
+    it.only('CPF incorreto', ()=>{
         cy.viewport(1440, 900)
         cy.visit('https://buger-eats.vercel.app')
 
         cy.get('a[href="/deliver"]').click()
         cy.get('#page-deliver form h1').should('have.text' , 'Cadastre-se para  fazer entregas')
 
-        var entregador = {
-            nome: 'James Rodriguez',
+        var deliver = {
+            name: 'James Rodriguez',
             cpf: '51',
             email:'james@gmail.com',
-            numero: '62982142744',
-            endereco: {
-                cep: '75083460',
-                rua: 'Rua Doutor Evandro Pinto Silva',
-                numero: '20',
-                bairro: 'Cidade Universitária',
-                complemento: 'AP 23',
-                cidade_uf: 'Anápolis/GO'
+            number: '62982142744',
+            address: {
+                postalcode: '75083460',
+                street: 'Rua Doutor Evandro Pinto Silva',
+                number: '20',
+                details: 'Cidade Universitária',
+                district: 'AP 23',
+                city_state: 'Anápolis/GO'
             },
 
-            metodo_entrega: 'Moto',
+               deliver_method: 'Moto',  
             cnh:'CNH-E.png'
 
         }
 
-        cy.get('input[name="name"]').type(entregador.nome)
-        cy.get('input[name="cpf"]').type(entregador.cpf)
-        cy.get('input[name="email"]').type(entregador.email)
-        cy.get('input[name="whatsapp"]').type(entregador.numero)
+        cy.get('input[name="name"]').type(deliver.name)
+        cy.get('input[name="cpf"]').type(deliver.cpf)
+        cy.get('input[name="email"]').type(deliver.email)
+        cy.get('input[name="whatsapp"]').type(deliver.number)
 
-        cy.get('input[name="postalcode"]').type(entregador.endereco.cep)
+        cy.get('input[name="postalcode"]').type(deliver.address.postalcode)
         cy.get('input[value="Buscar CEP"]').click()
         cy.wait(2000)
 
-        cy.get('input[name="address-number"]').type(entregador.endereco.numero)
-        cy.get('input[name="address-details"]').type(entregador.endereco.complemento)
+        cy.get('input[name="address-number"]').type(deliver.address.number)
+        cy.get('input[name="address-details"]').type(deliver.address.details)
 
-        cy.get('input[name="address"]').should('have.value',entregador.endereco.rua)
-        cy.get('input[name="district"]').should('have.value',entregador.endereco.bairro)
-        cy.get('input[name="city-uf"]').should('have.value',entregador.endereco.cidade_uf)
+        cy.get('input[name="address"]').should('have.value',deliver.address.street)
+        cy.get('input[name="district"]').should('have.value',deliver.address.details)
+        cy.get('input[name="city-uf"]').should('have.value',deliver.address.city_state)
 
 
-        cy.contains('.delivery-method li', entregador.metodo_entrega).click()
+        cy.contains('.delivery-method li', deliver.deliver_method).click()
         // cy.get('ul>li>[alt="Moto"]').click()
         // cy.get('ul>li>[alt="Bicicleta"]').click()
         // cy.get('ul>li>[alt="Van/Carro"]').click()
 
-        cy.get('input[accept^="image"]').attachFile(entregador.cnh)
+        cy.get('input[accept^="image"]').attachFile(deliver.cnh)
 
         cy.get('form button[type="submit"]').click()
 
